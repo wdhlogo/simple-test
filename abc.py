3053776c5f52d63d354998273aab59f0b87bb853
wdhlogo <wdhlogo@126.com>
2018-10-23 11:41:03 +0800

    abc.py

--- abc.py
+++ abc.py
@@ -1,15 +1,43 @@
 
 from selenium import webdriver
 import time
+import xlrd
 
-	
+#增加注释 
 def Login_url():
-
-	driver=webdriver.Chrome()
-	"""driver=webdriver.Ie()"""
-	username='wdhlogo'
-	passw='wdhlogo336*'
-	url='https://mail.126.com/'
+	
+	#打开一个workbook
+	workbook = xlrd.open_workbook('D:\\python_jenkins\\simple-test\\dataforlogin.xls')
+	#抓取所有sheet页的名称
+	worksheets = workbook.sheet_names()
+	print('worksheets is %s' %worksheets)
+	#定位到sheet1
+	worksheet1 = workbook.sheet_by_name(u'Sheet1')
+	"""
+	#遍历sheet1中所有行row
+	num_rows = worksheet1.nrows
+	for curr_row in range(num_rows):
+		row = worksheet1.row_values(curr_row)
+		print('row%s is %s' %(curr_row,row))
+	#遍历sheet1中所有列col
+	num_cols = worksheet1.ncols
+	for curr_col in range(num_cols):
+		col = worksheet1.col_values(curr_col)
+		print('col%s is %s' %(curr_col,col))
+	#遍历sheet1中所有单元格cell
+	for rown in range(num_rows):
+		for coln in range(num_cols):
+			cell = worksheet1.cell_value(rown,coln)
+			print (cell)
+				
+	"""		
+	#driver=webdriver.Chrome()
+	#driver=webdriver.Ie()
+	driver=webdriver.Ie()
+	#改成从excel里读出来
+	username=worksheet1.cell_value(1,1)
+	passw=worksheet1.cell_value(1,2)
+	url=worksheet1.cell_value(1,0)
 	driver.get(url)
 	time.sleep(1)
 	elem=driver.find_element_by_id("lbNormal")
